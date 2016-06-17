8c0a1332426f43eff0ae124cf0d820ca7a0efd60
JINWOO-J <jinwoo@yellotravel.com>
2016-06-17 11:07:25 +0900

    ....

--- nginx/files/run.sh
+++ nginx/files/run.sh
@@ -3,7 +3,7 @@ export UPSTREAM=${UPSTREAM:-"php_container:9000"}
 export DOMAIN=${DOMAIN:-"localhost"}
 export LOCATION=${LOCATION:-"#ADD_LOCATION"}
 export USE_DOCKERIZE=${USE_DOCKERIZE:-"yes"}
-
+export WEBROOT=${WEBROOT:-"/var/www/public"}
 if [ $USE_DOCKERIZE == "yes" ];
 then
     echo "USE the dockerize template";
